94c41abcaf9f9c9b388cb38c8454bb741e4dfe8f
github username <github email address>
2019-08-17 15:09:54 -0400

    Done.

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ function increaseRankBy(n) {
   }
 }
 
+// function deepestChild() {
+//   return document.querySelector('#grand-node div div div div');
+// }
+
 function deepestChild() {
-  return document.querySelector('#grand-node div div div div');
+  let node = document.getElementById( 'grand-node' )
+  let nextNode = node.children[ 0 ]
+
+  while ( nextNode ) {
+    node = nextNode
+    nextNode = node.children[ 0 ]
+  }
+
+  return node
 }
